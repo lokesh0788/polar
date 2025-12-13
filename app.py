f9d4e86fd8b4dabd74c2fbf82dad6762bde1a484
lokeshcse078 <s.lokeeshkumar006@gmail.com>
2025-12-13 23:41:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,6 +56,6 @@ def login_api():
 
 # ---------------- Main ---------------- #
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
+
 
